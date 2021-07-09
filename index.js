a8dc07a19a6751fda77eb52876c8ae0c9700cfe3
Quentin Chapon <q.chapon@gmail.com>
2021-07-09 16:14:44 +0200

    Big changes

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 // La ligne suivante ne doit être utilisée qu'une seule fois et au tout début du projet. De préférence dans index.js
 require("dotenv").config(); // Permet d'activer les variables d'environnement qui se trouvent dans le fichier `.env`
 
-mongoose.connect(process.env.MONGODB_URI); // Vous pourrez vous connecter à votre base de données, sans pour autant préciser les identifiants dans le fichier index.js
-
-//Cette ligne fait bénifier de CORS à toutes les requêtes de notre serveur
-app.use(cors());
-
 const express = require("express");
 const app = express();
 const formidable = require("express-formidable");
 const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGODB_URI); // Vous pourrez vous connecter à votre base de données, sans pour autant préciser les identifiants dans le fichier index.js
+//Cette ligne fait bénifier de CORS à toutes les requêtes de notre serveur
+app.use(cors());
 const morgan = require("morgan");
 app.use(formidable());
 app.use(morgan("dev"));
