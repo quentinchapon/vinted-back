d6dc89481adaf4ce11f31c778a7c7df2688bf304
Quentin Chapon <q.chapon@gmail.com>
2021-08-02 14:55:17 +0200

    Added required input for publish form

--- models/Offer.js
+++ models/Offer.js
@@ -1,11 +1,15 @@
 const mongoose = require("mongoose");
 
 const Offer = mongoose.model("Offer", {
-  product_name: String,
-  product_description: String,
-  product_price: Number,
+  product_name: { type: String, require: true },
+  product_description: { type: String, require: true },
+  product_price: { type: String, require: true },
   product_details: Array,
-  product_image: { type: mongoose.Schema.Types.Mixed, default: {} },
+  product_image: {
+    type: mongoose.Schema.Types.Mixed,
+    default: {},
+    require: true,
+  },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
